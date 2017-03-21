f032863b7f29674c82afb666ea5f938a3e9e0bc4
mashi100 <niponjon@gmail.com>
2017-03-21 12:34:36 +0700

    again

--- bot.php
+++ bot.php
@@ -38,7 +38,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',				
-				'text' => $t1;
+				'text' => $t1
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
