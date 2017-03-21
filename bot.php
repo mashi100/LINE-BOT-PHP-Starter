3f95bdfac29c03ea961596295c9c04cce3b6a612
mashi100 <niponjon@gmail.com>
2017-03-21 12:29:08 +0700

    find user ID

--- bot.php
+++ bot.php
@@ -15,6 +15,8 @@ if (!is_null($events['events'])) {
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
+			// Get User ID
+			$u = $event['source']['userId'];
 
 			// find time
 			$param = array(
@@ -36,7 +38,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $t1
+				'text' => $t1.'-'.$u;
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
